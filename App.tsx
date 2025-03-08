5291946f88114ebca1f9ea717215580b49a10c9f
민대인 <bamin0422@gmail.com>
2025-03-09 08:33:32 +0900

    feat: Setup Empty Component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import DateHead from './components/DateHead/ui';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import Empty from './components/Empty/ui';
+import {StyleSheet} from 'react-native';
 
-function App(): React.JSX.Element {
+const App = (): React.JSX.Element => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead />
+        <Empty />
       </SafeAreaView>
     </SafeAreaProvider>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ components/Empty/ui/index.tsx
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const Empty = () => {
+  return (
+    <View style={styles.block}>
+      <Text style={styles.description}>오늘 할일이 존재하지 않습니다.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  description: {
+    fontSize: 24,
+    color: '#9e9e9e',
+  },
+});
+
+export default Empty;
